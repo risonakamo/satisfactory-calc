711bc81b16af09e3162a8a5388aa420055dafef0
risonakamo <hmykanae@gmail.com>
2024-12-07 14:31:40 -0500

    raw resource calc working

--- lib/satisfactory_calc/raw_resource.go
+++ lib/satisfactory_calc/raw_resource.go
@@ -1,12 +1,28 @@
 package satisfactory_calc
 
+import (
+	"satisfactory-calc/lib/utils"
+
+	"k8s.io/apimachinery/pkg/util/sets"
+)
+
 // calculate total resource use of a factory given target resources
 // output is inputs dict which doubles as outputs dict.
-// func calculateResourceUse(fac Factory,targetResources sets.Set[string]) InputsDict {
-//     var result InputsDict=InputsDict{}
+func calculateResourceUse(fac Factory,targetResources sets.Set[string]) InputsDict {
+    var result InputsDict=InputsDict{}
 
-//     if targetResources.Has(fac.ItemName) {
-//         result[fac.ItemName]=fac.TotalOutput
-//     }
+    if targetResources.Has(fac.ItemName) {
+        result[fac.ItemName]=fac.TotalOutput
+    }
 
-// }
+    var subFactDict FactorybyRecipe
+    for _,subFactDict = range fac.SubFactories {
+        var subFactory Factory=utils.GetDictFirstItem(subFactDict)
+        result=mergeInputDict(
+            result,
+            calculateResourceUse(subFactory,targetResources),
+        )
+    }
+
+    return result
+}
---
+++ lib/satisfactory_calc/raw_resource_test.go
@@ -0,0 +1,42 @@
+package satisfactory_calc
+
+import (
+	"testing"
+
+	"github.com/k0kubun/pp/v3"
+	"k8s.io/apimachinery/pkg/util/sets"
+)
+
+func Test_calcResource(t *testing.T) {
+	data := loadRecipesDict("../../data/factorylab_data.json")
+	fac := createFactory(data["heavy-modular-frame"]["Heavy Modular Frame"])
+	fac, e := constructFactory2(fac, data, []string{
+		"Steel Pipe",
+		"Steel Ingot",
+		"Modular Frame",
+		"Reinforced Iron Plate",
+		"Iron Plate",
+		"Iron Rod",
+		"Iron Ingot",
+		"Iron Ore",
+		"Screw",
+		"Coal",
+		"Encased Industrial Beam",
+		"Concrete",
+		"Steel Beam",
+		"Limestone",
+	})
+
+	if e != nil {
+		t.Error(e)
+	}
+
+
+    result:=calculateResourceUse(fac,sets.New([]string{
+        "iron-ore",
+        "coal",
+        "limestone",
+    }...))
+
+    pp.Println(result)
+}
